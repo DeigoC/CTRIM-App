affe571f7aff6de17bc0afee8f28ca1fad4e68ac
DeigoC <diegocollado117@gmail.com>
2020-08-02 17:24:29 +0100

    Ready for first test release?

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,5 @@ void main() {
   runApp(App());
 }
 
+//keytool -genkey -v -keystore c:\Users\diego\key.jks -storetype JKS -keyalg RSA -keysize 2048 -validity 10000 -alias key
+//C:\Program Files\Android\Android Studio\jre\bin
